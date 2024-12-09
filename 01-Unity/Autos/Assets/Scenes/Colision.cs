0ff73bdddf17d6e9a202c6d839220c8937a642bc
David Alexander Averos <david.averos@epn.edu.ec>
2024-12-09 17:28:45 -0500

    Clase 5

--- 01-Unity/Autos/Assets/Scenes/Colision.cs
+++ 01-Unity/Autos/Assets/Scenes/Colision.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Colision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("AUUCH");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entrando al trigger");
+        if (other.tag == "Damage")
+        {
+            Debug.Log("-1000 aura");
+        }
+
+        if (other.tag == "Paquete")
+        {
+            Debug.Log("Paquete recogido");
+        }
     }
 }
