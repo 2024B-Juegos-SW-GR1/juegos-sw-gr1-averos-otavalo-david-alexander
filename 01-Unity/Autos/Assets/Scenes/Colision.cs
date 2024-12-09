3c6717a099c337a2d267caf080a277fa4a5a8a1e
David Alexander Averos <david.averos@epn.edu.ec>
2024-12-09 18:41:45 -0500

    Clase 6

--- 01-Unity/Autos/Assets/Scenes/Colision.cs
+++ 01-Unity/Autos/Assets/Scenes/Colision.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 
 public class Colision : MonoBehaviour
 {
+    [SerializeField] private float destroyDelay = 0.5f;
+    private bool hasPackage;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("AUUCH");
@@ -12,15 +21,19 @@ public class Colision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Entrando al trigger");
-        if (other.tag == "Damage")
+        Debug.Log("Entrando al trigger con:" + other.tag + "con el nombre de objeto:" + other.name);
+        if (other.CompareTag("Damage") && hasPackage != true)
         {
             Debug.Log("-1000 aura");
+            hasPackage = false;
+            _spriteRenderer.color = Color.green;
         }
 
-        if (other.tag == "Paquete")
+        if (other.CompareTag("paquete"))
         {
             Debug.Log("Paquete recogido");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
     }
 }
