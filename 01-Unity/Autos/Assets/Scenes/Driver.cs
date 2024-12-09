8fe8c9bb992c206891b3834636f336c0ecc16787
David Alexander Averos <david.averos@epn.edu.ec>
2024-12-08 21:46:23 -0500

    Clase 04

--- 01-Unity/Autos/Assets/Scenes/Driver.cs
+++ 01-Unity/Autos/Assets/Scenes/Driver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 45);
+        float steerArmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveArmount = Input.GetAxis("Vertical") * moveSpeed;
+        
+        transform.Rotate(0, 0, steerArmount);
+        transform.Translate(0,moveArmount,0);
     }
 }
